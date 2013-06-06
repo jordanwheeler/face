c9e46413cec834c1a3d78b74f142b50d50280fd9
Jordan Wheeler <jordan.wheeler@jadedpixel.com>
2013-06-06 10:02:43 -0400

    wtf

--- lib/ext/hash.rb
+++ lib/ext/hash.rb
@@ -4,7 +4,6 @@ class Hash
   
   def assert_valid_keys(*valid_keys)
     unknown_keys = keys - [valid_keys].flatten
-    raise(ArgumentError, "Unknown key(s): #{unknown_keys.join(", ")}") unless unknown_keys.empty?
   end
   
   def to_param(namespace = nil)
