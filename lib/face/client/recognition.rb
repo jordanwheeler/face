203bd2b5cdd401f34f0130d84ee43c8468b7a61e
Jordan Wheeler <jordan.wheeler@jadedpixel.com>
2013-06-06 09:49:53 -0400

    remove assert_valid_keys for faces_group

--- lib/face/client/recognition.rb
+++ lib/face/client/recognition.rb
@@ -7,7 +7,6 @@ module Face
       end
 
       def faces_group(opts={})
-        opts.assert_valid_keys(:uids, :urls, :file, :namespace, :detector, :attributes, :callback, :callback_url)
         make_request(:faces_group, opts)
       end
 
